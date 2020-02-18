395ca1b2a3de0797a2062dab6d61641891e42262
Hlo1 <hailong211975@gmail.com>
2020-02-18 09:51:20 +0700

    initial commit

---
+++ homework8/hw8.js
@@ -0,0 +1,20 @@
+// let link = document.getElementById("demo-a")
+// let btn = document.getElementById("btn-demo")
+// let text = document.getElementById("input-demo")
+
+// btn.addEventListener('click',()=>{
+//     console.dir(text)
+//     console.dir(link)
+//     text.value += link.href;
+// })
+
+// let color = document.getElementById("colorSelect")
+// let button = document.getElementById("button")
+// button.addEventListener("click",()=>{
+//     console.dir(color)
+//     color.remove(color.selectedIndex)
+// })
+
+
+
+
